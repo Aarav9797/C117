55d5ba05009f198fd40719ad064eaded0e1c87cb
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-04-06 17:55:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,10 @@
-var nose_x = 0
-var nose_y = 0
-let img;
-function preload(){
-    img=loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png")
+function setup(){
+canvas = createCanvas(300,300)
+    canvas.center()
+capture=createCapture(VIDEO)
+capture.hide()
 }
-
-function setup() {
-    canvas = createCanvas(300, 300);
-    canvas.center();
-    capture = createCapture(VIDEO);
-    capture.size(300, 300);
-    capture.hide();
-    postnet = ml5.poseNet(capture, modeloaded)
-    postnet.on("pose", displayresults)
+function draw(){
+    image(capture,0,0,300,300)
 }
-
-function draw() {
-    image(capture, 0, 0, 300, 300);
-    image(img, nose_x, nose_y, 30, 30);
-
-
-}
-
-function modeloaded() {
-    console.log("The model has been loaded")
-}
-
-function displayresults(results) {
-    if (results.length > 0) {
-        console.log(results[0].pose.nose)
-        nose_x = results[0].pose.nose.x-5;
-        nose_y = results[0].pose.nose.y-5;
-
-    }
-
-}
+function preload(){}
