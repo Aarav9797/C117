55f756cf9280d518ce0c91536b01f75991b1be74
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-04-10 18:56:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-function setup(){
-canvas = createCanvas(300,300)
+function setup() {
+    canvas = createCanvas(300, 300)
     canvas.center()
-capture=createCapture(VIDEO)
-capture.hide()
+    capture = createCapture(VIDEO)
+    capture.hide()
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Eof6uq_2e/model.json", modelloaded)
 }
-function draw(){
-    image(capture,0,0,300,300)
+function modelloaded(){
+    console.log("The Model has been loaded")
 }
-function preload(){}
+function draw() {
+    image(capture, 0, 0, 300, 300);
+ classifier.classify(capture, getresults)
+}
+
+function preload() {}
+function getresults(error,results){
+   console.log(results) 
+   document.getElementById("obj").innerHTML=results[0].label;
+   document.getElementById("acc").innerHTML=results[0].confidence.toFixed(3);
+}
